4f9a2993697e2ad50320af479ed3cfbdfe4bbbfa
Your Name <you@example.com>
2023-12-02 16:27:33 +0300

    feat: working on adding,deleting and update propertes

--- app/Http/Controllers/Properties/PropertyController.php
+++ app/Http/Controllers/Properties/PropertyController.php
@@ -2,66 +2,116 @@
 
 namespace App\Http\Controllers\Properties;
 
-use App\Http\Controllers\Controller;
-use App\Models\Properties\Property;
+use Auth;
 use Illuminate\Http\Request;
+use App\Models\Categories\Category;
+use App\Models\Properties\Property;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Session;
 
-class PropertyController extends Controller
-{
+class PropertyController extends Controller {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $properties = Property::where('user_id',auth()->user()->id)->latest()->paginate(10);
-        return view('backend.properties.index',compact('properties'));
+    * Display a listing of the resource.
+    */
+
+    public function index() {
+        $properties = Property::where( 'user_id', auth()->user()->id )->latest()->paginate( 10 );
+        return view( 'backend.properties.index', compact( 'properties' ) );
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    * Show the form for creating a new resource.
+    */
+
+    public function create() {
+        $categories = Category::all();
+        return view( 'backend.properties.create', compact( 'categories' ) );
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    * Store a newly created resource in storage.
+    */
+
+    public function store( Request $request ) {
+        $property = new Property();
+        $property->name = $request->name;
+        $property->description = $request->description;
+        $property->category_id = $request->category_id;
+        $property->user_id = auth()->user()->id;
+        
+        try {
+            $property->save();
+            Session::flash( 'success', 'Property successfully created' );
+            return redirect()->route( 'properties.index' );
+        } catch ( QueryException $exception ) {
+            Session::flash( 'error', 'Failed to create property' );
+            return back();
+        }
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Property $property)
-    {
-        //
+    * Display the specified resource.
+    */
+
+    public function show( string $id ) {
+        $property = Property::find($id);
+        if (!$property) {
+            ession::flash('error', 'Property not found');
+            return back();
+        }
+        return view('backend.properties.show',compact('property'));
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Property $property)
-    {
-        //
+    * Show the form for editing the specified resource.
+    */
+
+    public function edit( $id ) {
+        $property = Property::find($id);
+        if (!$property) {
+            ession::flash('error', 'Property not found');
+            return back();
+        }
+        $categories = Category::where('id', '!=', $property->category_id)->get();
+        return view('backend.properties.edit',compact('property','categories'));
     }
 
     /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Property $property)
-    {
-        //
+    * Update the specified resource in storage.
+    */
+
+    public function update( Request $request, string $id ) {
+        $property = Property::find($id);
+        if (!$property) {
+            Session::flash('error', 'Property not found');
+            return back();
+        }
+        $property->name = $request->name;
+        $property->category_id = $request->category_id;
+        $property->description = $request->description;
+        $property->save();
+        Session::flash('success', 'Property successfully updated');
+        return redirect()->route('properties.index');
     }
 
     /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Property $property)
-    {
-        //
+    * Remove the specified resource from storage.
+    */
+
+    public function destroy( string $id ) {
+        $property = Property::find($id);
+        if (!$property) {
+            Session::flash('error', 'Property not found');
+            return back();
+        }
+        try {
+            $property->delete();
+            Session::flash('success', 'Property deleted successfully');
+            return redirect()->route('properties.index');
+        } catch (QueryException $exception) {
+            Session::flash('error', 'Failed to be deleted');
+            return back();
+        }
     }
 }
--- resources/views/backend/properties/edit.blade.php
+++ resources/views/backend/properties/edit.blade.php
@@ -1,3 +1,71 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+@extends('backend.layouts.app')
+@section('content')
+@php
+        $activeFlag = 0;
+    @endphp
+    <!-- Main content -->
+    <section class="content pt-5">
+        <div class="container-fluid">
+            <div class="col-md-8 offset-md-2">
+                <!-- general form elements -->
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Edit Property</h4>
+                  </div>
+                  <!-- /.card-header -->
+                  <!-- form start -->
+                  <form action="{{ route('properties.update',$property->id ) }}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-6 mb-2">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" value="{{ $property->name }}" required>
+                                @if ($errors->has('name'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <div class="form-group">
+                                <label for="">Category</label>
+                                <select name="category_id" id="" class="form-control" required>
+                                    <option value="{{ $property->category->id }}" > {{ $property->category->name }} </option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12 mb-12">
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea name="description" class="form-control" cols="30" rows="4">{{ $property->description }}</textarea>
+                                @if ($errors->has('description'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('description') }}</strong>
+                                </span>
+                            @endif
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer d-flex">
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                      <a class="btn btn-danger mx-3" href="{{ route('properties.index') }}">Back</a>
+                    </div>
+                  </form>
+                </div>
+                <!-- /.card -->    
+              </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
